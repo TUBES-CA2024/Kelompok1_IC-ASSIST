34252e66c768254b8c31cfac067e914b351a0497
acoramadan <laxmiwatimaani@gmail.com>
2024-10-02 23:37:47 +0800

    finish for today idk what im doing

--- app/View/Templates/presentasi.php
+++ app/View/Templates/presentasi.php
@@ -64,6 +64,9 @@
             <td><a href="#">download</a></td>
             <td>Silahkan submit ppt dan  makalahnya!</td>
           </tr>
+          </tbody>
+          </table>
+          </div>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
